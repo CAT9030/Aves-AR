a62fe693b48daf842a71aa96bd4bac3d48ec79f1
Kazura-Collins <98463668+Kazura-Collins@users.noreply.github.com>
2024-11-27 16:04:48 -0500

    Sporophila minuta
    Isaac Montaño

---
+++ Sporophila minuta/script.js
@@ -0,0 +1,27 @@
+
+const onProgress = (event) => {
+  const progressBar = event.target.querySelector('.progress-bar');
+  const updatingBar = event.target.querySelector('.update-bar');
+  updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
+  if (event.detail.totalProgress === 1) {
+    progressBar.classList.add('hide');
+    event.target.removeEventListener('progress', onProgress);
+  } else {
+    progressBar.classList.remove('hide');
+  }
+};
+
+const modelViewer = document.querySelector('#ave');
+document.querySelector('model-viewer').addEventListener('progress', onProgress);
+document.querySelector('#cambioAnimation').onclick = function(){
+  animationChange()
+}
+
+document.querySelector('#cambioAnimation').ontouchstart = function(){
+  animationChange()
+}
+
+function animationChange(){
+  modelViewer.animationName = modelViewer.animationName === 'Idle' ?
+      'Volar' : 'Idle';
+}
